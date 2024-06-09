b06a16ec188b00f2db464f00677df64b11ea0b3b
w40141 <w.40141@gmail.com>
2024-06-09 22:48:06 +0900

    format sql

--- docker/db/data/01_create_table.sql
+++ docker/db/data/01_create_table.sql
@@ -1,28 +1,24 @@
 START TRANSACTION;
 
 -- --------------------------------------------------------
---
--- テーブルの構造 `user`
---
-CREATE TABLE
-  IF NOT EXISTS `user` (
-    `id` VARCHAR(26) PRIMARY KEY NOT NULL,
-    `name` varchar(30) NOT NULL
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
+-- テーブルの構造 `users`
+-- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` VARCHAR(26) PRIMARY KEY,
+  `name` VARCHAR(30) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
 
 -- --------------------------------------------------------
---
 -- テーブルの構造 `todo`
---
-CREATE TABLE
-  IF NOT EXISTS `todo` (
-    `id` VARCHAR(26) PRIMARY KEY NOT NULL,
-    `title` varchar(200) NOT NULL,
-    `status` int NOT NULL DEFAULT 0,
-    `user_id` VARCHAR(26) NOT NULL,
-    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY fk_todo_in_user_id (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
+-- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `todos` (
+  `id` VARCHAR(26) PRIMARY KEY,
+  `title` VARCHAR(200) NOT NULL,
+  `status` INT NOT NULL DEFAULT 0,
+  `user_id` VARCHAR(26) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
 
 COMMIT;
